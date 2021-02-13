20cc96e291aff0724c744fbc2e6a095f58741d84
Armond W <armond.willingham@rev.com>
2021-02-13 15:20:57 -0600

    Basic setup

---
+++ Web/FrontEnd/awillingham-web-app/webpack/webpack.dev.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '../../../wwwroot/js/bundles'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  }
+};
