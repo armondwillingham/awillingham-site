a5f44ece8de74a9292103f655df32a77decc927b
Armond W <armond.willingham@rev.com>
2021-10-30 18:26:04 -0500

    Fancybrot

--- Web/FrontEnd/awillingham-web-app/src/Components/Sketches/Mandlebrot/Mandlebrot.tsx
+++ Web/FrontEnd/awillingham-web-app/src/Components/Sketches/Mandlebrot/Mandlebrot.tsx
@@ -1,10 +1,10 @@
 import P5 from 'p5';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { hexToRgb } from 'Utils/HexToRgb';
 import { SketchPreviewProps } from '../SketchTypes';
 
-const MAX_ITER = 50;
+const MAX_ITER = 10;
 
 interface TestSketchState {
     canvasRef: React.RefObject<HTMLDivElement>;
@@ -45,8 +45,11 @@ class Complex {
     }
 }
 
-export class MandlebrotSketch extends React.PureComponent<SketchPreviewProps, TestSketchState> {
-    constructor(props: SketchPreviewProps) {
+interface MandlebrotSketchProps extends SketchPreviewProps {
+    zoom: number;
+}
+export class MandlebrotSketch extends React.PureComponent<MandlebrotSketchProps, TestSketchState> {
+    constructor(props: MandlebrotSketchProps) {
         super(props);
         this.state = {
             canvasRef: React.createRef(),
@@ -69,22 +72,22 @@ export class MandlebrotSketch extends React.PureComponent<SketchPreviewProps, Te
         let prevMouseX = 0;
         let prevMouseY = 0;
 
-        const scale = 1;
         p5.setup = (): void => {
             p5.createCanvas(W, H);
-            p5.colorMode(p5.HSB);
         };
 
         p5.draw = (): void => {
             const backgroundColor = hexToRgb(this.props.theme.backgroundColor.primary);
             p5.background(backgroundColor.r, backgroundColor.g, backgroundColor.b);
+            p5.colorMode(p5.HSB);
+            const zoom = this.props.zoom;
             for (let i = 0; i < W; ++i) {
                 for (let j = 0; j < H; ++j) {
-                    const x = p5.map(i + translateX, 0, W, -2, 1);
-                    const y = p5.map(j + translateY, 0, H, -1.5, 1.5);
+                    const x = p5.map(i + translateX * zoom, 0, W, -2 / zoom, 1 / zoom);
+                    const y = p5.map(j + translateY * zoom, 0, H, -1.5 / zoom, 1.5 / zoom);
                     const mandlebrot = this.calculateMandlebrot(x, y);
-                    const color = p5.map(mandlebrot, 1, MAX_ITER, 0, 255);
-                    p5.set(i, j, color);
+                    const color = p5.map(mandlebrot, 1, MAX_ITER * zoom, 0, 255);
+                    p5.set(i, j, p5.color(color, 100, 100));
                 }
             }
             p5.updatePixels();
@@ -96,27 +99,25 @@ export class MandlebrotSketch extends React.PureComponent<SketchPreviewProps, Te
             }
             prevMouseX = e.clientX;
             prevMouseY = e.clientY;
-            console.log({ prevMouseX, prevMouseY });
         };
 
         p5.mouseDragged = (e: any): void => {
             if (e.clientX < 0 || e.clientX > W || e.clientY < 0 || e.clientY > H) {
                 return;
             }
-            const dx = prevMouseX - e.clientX;
-            const dy = prevMouseY - e.clientY;
+            const dx = (prevMouseX - e.clientX) / this.props.zoom;
+            const dy = (prevMouseY - e.clientY) / this.props.zoom;
             translateX += dx;
             translateY += dy;
             prevMouseX = e.clientX;
             prevMouseY = e.clientY;
-            console.log(translateX);
         };
     };
 
     calculateMandlebrot(x: number, y: number): number {
         const z = new Complex(0, 0);
         let n = 0;
-        while (z.abs() <= 2 && n < MAX_ITER) {
+        while (z.abs() <= 2 && n < MAX_ITER * this.props.zoom) {
             z.square().add(x, y);
             ++n;
         }
@@ -129,11 +130,40 @@ export class MandlebrotSketch extends React.PureComponent<SketchPreviewProps, Te
 }
 
 export const MandlebrotSketchComponent = (props: SketchPreviewProps): JSX.Element => {
+    const [zoom, setZoom] = useState(1);
+    const [deltaZoom, setDeltaZoom] = useState(0);
+    const [isZooming, setIsZooming] = useState(false);
     const W = 500;
     const H = 500;
-    return <MandlebrotSketch {...props} width={W} height={H} />;
+
+    const startZoom = useCallback((amt: number) => {
+        setIsZooming(true);
+        setDeltaZoom(amt);
+    }, []);
+
+    const stopZoom = useCallback((): void => {
+        setIsZooming(false);
+    }, []);
+
+    useEffect(() => {
+        if (!isZooming) return;
+        setZoom(zoom + deltaZoom);
+    }, [isZooming, zoom, deltaZoom]);
+
+    return (
+        <div>
+            <MandlebrotSketch {...props} width={W} height={H} zoom={zoom} />
+            <button onMouseDown={() => startZoom(0.25)} onMouseUp={() => stopZoom()}>
+                Zoom in
+            </button>
+            <button onMouseDown={() => startZoom(-0.25)} onMouseUp={() => stopZoom()}>
+                Zoom out
+            </button>
+            <div>Zoom: {zoom}</div>
+        </div>
+    );
 };
 
 export const MandlebrotPreview = (props: SketchPreviewProps): JSX.Element => {
-    return <MandlebrotSketch {...props} />;
+    return <MandlebrotSketch {...props} zoom={1} />;
 };
