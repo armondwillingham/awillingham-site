dc090fe0ef4da7c0d956bc8f8aa664cb6caa84c0
Armond W <armond.willingham@rev.com>
2021-11-05 19:00:12 -0500

    try hashing

--- Controllers/SourceUpdatedController.cs
+++ Controllers/SourceUpdatedController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -6,6 +9,7 @@ using Microsoft.Extensions.Options;
 using awillingham_site.Models;
 using awillingham_site.Extensions;
 using awillingham_site.Config;
+using System.Text;
 
 namespace awillingham_site.Controllers
 {
@@ -26,9 +30,30 @@ namespace awillingham_site.Controllers
         [HttpPost]
         [Route("sourceupdated")]
         public IActionResult SourceUpdated()
-        {
-            _logger.LogRequest(_appOptionsMonitor.CurrentValue.GithubSecret, Request);
+        {   
+            var streamReader = new StreamReader(Request.Body);
+            var body = streamReader.ReadToEnd();
+            var hash = GetHash(body, _appOptionsMonitor.CurrentValue.GithubSecret);
+            _logger.LogRequest(hash, Request);
+            
             return Ok();
+        }
+
+        private string GetHash(string text, string key)
+        {
+            // change according to your needs, an UTF8Encoding
+            // could be more suitable in certain situations
+            ASCIIEncoding encoding = new ASCIIEncoding();
+
+            byte[] textBytes = encoding.GetBytes(text);
+            byte[] keyBytes = encoding.GetBytes(key);
+
+            byte[] hashBytes;
+
+            using (HMACSHA256 hash = new HMACSHA256(keyBytes))
+                hashBytes = hash.ComputeHash(textBytes);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
